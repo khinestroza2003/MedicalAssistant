412fe20ee000eb4254b7967695610de9ac565e50
khinestroza2003 <kevin.hinestroza30@gmail.com>
2025-11-10 16:10:01 -0500

    creacion de backend

---
+++ backend/backend_asistentemedico/src/main/java/com/asistente_medico/back/backend_asistentemedico/BackendAsistentemedicoApplication.java
@@ -0,0 +1,13 @@
+package com.asistente_medico.back.backend_asistentemedico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAsistentemedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAsistentemedicoApplication.class, args);
+	}
+
+}
---
+++ backend/backend_asistentemedico/src/test/java/com/asistente_medico/back/backend_asistentemedico/BackendAsistentemedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asistente_medico.back.backend_asistentemedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAsistentemedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
